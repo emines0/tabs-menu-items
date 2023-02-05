75217e1f0b9d49543506a43821435cd841a46357
emines0 <emin.sefa1@gmail.com>
2023-02-05 13:44:38 +0100

    job data display

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-#### IN ACTION
+#### Tab menu items
 
 [Portfolio](https://gatsby-strapi-portfolio-project.netlify.app/)
+
+- fetch data form API
+- useState for loading, jobs
+- return html based on state condition
+-
--- src/App.js
+++ src/App.js
@@ -4,7 +4,72 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url)
+      const newJobs = await response.json()
+      setJobs(newJobs)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+
+  /*
+   * Destructuring of the props needs to happen after loading
+   * Before loading is jobs an empty array
+   */
+
+  const { company, dates, duties, title } = jobs[value]
+
+  /*
+   * jobs[value] - which job from the list want to display
+   */
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div className='job-desc' key={index}>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button type='button' className='btn'>
+        more info
+      </button>
+    </section>
+  )
 }
 
 export default App
